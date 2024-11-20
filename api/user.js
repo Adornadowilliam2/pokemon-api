62d720bf5aae0942a8306b7cd1fbb775e1cf9701
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-11-21 05:45:18 +0800

    add this changes

--- api/user.js
+++ api/user.js
@@ -6,42 +6,42 @@ export default function handler(req, res) {
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Bulbasaur.png?raw=true",
       type: "grass",
-      environment: "grass",
+      color:["green", "violet"]
     },
     {
       name: "Ivysaur",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Ivysaur.png?raw=true",
       type: "grass",
-      environment: "grass",
+      color:["green", "violet"]
     },
     {
       name: "Venosaur",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Venusaur.png?raw=true",
       type: "grass",
-      environment: "grass",
+      color:["green", "violet"]
     },
     {
       name: "Charmander",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Charmander.png?raw=true",
       type: "fire",
-      environment: "hot",
+      color: ["red", "orangered"]
     },
     {
       name: "Charmeleon",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Charmeleon.png?raw=true",
       type: "fire",
-      environment: "hot",
+      color: ["red", "orangered"]
     },
     {
       name: "Charizard",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Charizard.png?raw=true",
       type: "fire",
-      environment: "hot",
+      color: ["red", "orangered"]
     },
     {
       name: "Squirtle",
@@ -49,27 +49,28 @@ export default function handler(req, res) {
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Squirtle.png?raw=true",
       type: "water",
       environment: "water",
+      color: ["blue", "lightblue"]
     },
     {
       name: "Wartortle",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Wartotle.png?raw=true",
       type: "water",
-      environment: "water",
+      color: ["blue", "lightblue"]
     },
     {
       name: "Blastoise",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Blastoise.png?raw=true",
       type: "water",
-      environment: "water",
+      color: ["blue", "lightblue"]
     },
     {
       name: "Caterpie",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Caterpie.png?raw=true",
       type: "grass",
-      environment: "grass",
+      color: ["green", "lightgreen"]
     },
   ];
   res.status(200).json(pokemonInfo);
