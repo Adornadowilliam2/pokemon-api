2dd762d3a2a44d061cd50814423f8e0c7371ae63
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-11-20 15:01:30 +0800

    fix the issue in cors

--- api/user.js
+++ api/user.js
@@ -1,10 +1,9 @@
 export default function handler(req, res) {
-    const userInfo = {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const userInfo = {
       name: "John Doe",
       age: 20,
       birthdate: "Rosario, Pasig City"
-    };
-  
-    res.status(200).json(userInfo);
-  }
-  
+  };
+  res.status(200).json(userInfo);
+}
