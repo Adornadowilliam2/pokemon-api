ed501a8148265b0422afab73a27e4a008619763b
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-11-22 12:47:09 +0800

    add changes

--- api/user.js
+++ api/user.js
@@ -2,75 +2,131 @@ export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   const pokemonInfo = [
     {
-      name: "Bulbasaur",
+      name: "bulbasaur",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Bulbasaur.png?raw=true",
       type: "grass",
-      color:["green", "violet"]
+      color: ["green", "violet"],
     },
     {
-      name: "Ivysaur",
+      name: "ivysaur",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Ivysaur.png?raw=true",
       type: "grass",
-      color:["green", "violet"]
+      color: ["green", "violet"],
     },
     {
-      name: "Venosaur",
+      name: "venosaur",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Venusaur.png?raw=true",
       type: "grass",
-      color:["green", "violet"]
+      color: ["green", "violet"],
     },
     {
-      name: "Charmander",
+      name: "charmander",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Charmander.png?raw=true",
       type: "fire",
-      color: ["red", "orangered"]
+      color: ["red", "orangered"],
     },
     {
-      name: "Charmeleon",
+      name: "charmeleon",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Charmeleon.png?raw=true",
       type: "fire",
-      color: ["red", "orangered"]
+      color: ["red", "orangered"],
     },
     {
-      name: "Charizard",
+      name: "charizard",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Charizard.png?raw=true",
       type: "fire",
-      color: ["red", "orangered"]
+      color: ["red", "orangered"],
     },
     {
-      name: "Squirtle",
+      name: "squirtle",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Squirtle.png?raw=true",
       type: "water",
       environment: "water",
-      color: ["blue", "lightblue"]
+      color: ["blue", "lightblue"],
     },
     {
-      name: "Wartortle",
+      name: "wartortle",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Wartotle.png?raw=true",
       type: "water",
-      color: ["blue", "lightblue"]
+      color: ["blue", "lightblue"],
     },
     {
-      name: "Blastoise",
+      name: "blastoise",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Blastoise.png?raw=true",
       type: "water",
-      color: ["blue", "lightblue"]
+      color: ["blue", "lightblue"],
     },
     {
-      name: "Caterpie",
+      name: "caterpie",
       image:
         "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Caterpie.png?raw=true",
-      type: "grass",
-      color: ["green", "lightgreen"]
+      type: "bug",
+      color: ["green", "lightgreen"],
+    },
+    {
+      name: "metapod",
+      image:
+        "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Metapod.png?raw=true",
+      type: "bug",
+      color: ["green", "lightgreen"],
+    },
+    {
+      name: "butterfree",
+      image:
+        "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Butterfree.png?raw=true",
+      type: "bug",
+      color: ["green", "lightgreen"],
+    },
+    {
+      name: "weedle",
+      image:
+        "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Weedle.png?raw=true",
+      type: "bug",
+      color: ["green", "lightgreen"],
+    },
+    {
+      name: "kakuna",
+      image:
+        "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Kakuna.png?raw=true",
+      type: "bug",
+      color: ["green", "lightgreen"],
+    },
+    {
+      name: "Beedrill",
+      image:
+        "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Beedrill.png?raw=true",
+      type: "bug",
+      color: ["green", "lightgreen"],
+    },
+    {
+      name: "Pidgey",
+      image:
+        "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Pidgey.png?raw=true",
+      type: "flying",
+      color: ["gray"],
+    },
+    {
+      name: "Pidgeotto",
+      image:
+        "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Pidgeotto.png?raw=true",
+      type: "flying",
+      color: ["gray"],
+    },
+    {
+      name: "Pidgeot",
+      image:
+        "https://github.com/Adornadowilliam2/pokemon-api/blob/images/Pidgeot.png?raw=true",
+      type: "flying",
+      color: ["gray"],
     },
   ];
   res.status(200).json(pokemonInfo);
